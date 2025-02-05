792b9b29c57fcf5b1b5bc10577a939cb7c1a0bfe
FlockFlows7-Dev <clarkmatthew707@gmail.com>
2025-02-04 18:01:28 -0600

    A simple C# program to gather user information.
    This program prompts the user for their name, age, and favorite thing, then displays the information back to them.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string name = "";
+        String favorite = "";
+        int age = 0;
+
+        Console.Write("whats your name: ");
+        name = (Console.ReadLine());
+        Console.Clear();
+
+        Console.WriteLine("whats your age: ");
+        age = int.Parse(Console.ReadLine());
+        Console.Clear();
+
+        Console.WriteLine("whats your favorite thing: ");
+        favorite = (Console.ReadLine());
+        Console.Clear();
+
+        Console.WriteLine("your name is " + name + " your age is " + age + "\n" + "and your favorite thing is " + favorite + "...");
+    }
+}
